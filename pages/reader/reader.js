302b6098944fd4f9b3f28c1848bd92d16ea566d4
hyq <1713877106@qq.com>
2019-02-18 17:34:34 +0800

    Initial Commit

---
+++ pages/reader/reader.js
@@ -0,0 +1,8 @@
+//reader.js
+//获取应用实例
+// const app = getApp()
+
+Page({
+  // data: {
+  // }
+})
---
+++ pages/welcome/welcome.js
@@ -0,0 +1,9 @@
+//welcome.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    text:"Hello,七月"
+  }
+})
