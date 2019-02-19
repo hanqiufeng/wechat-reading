14aa7aa336262e6bc626dbc3869985acb80efcdc
hyq <1713877106@qq.com>
2019-02-19 17:52:00 +0800

    完成文章列表

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,9 +1,27 @@
 //welcome.js
-//获取应用实例
-const app = getApp()
 
 Page({
   data: {
-    text:"Hello,七月"
+    text:"Hello,七月·安生"
+  },
+
+  onTap: function () {
+    // wx.navigateTo({
+    //   url: '../posts/post',
+    // });
+
+    wx.redirectTo({
+      url: '../posts/post',
+    })
+  },
+
+  onUnload:function(){
+    // console.log("onUnload")
+  },
+
+  onHide:function(){
+    // console.log("onHide")
   }
 })
+
+
