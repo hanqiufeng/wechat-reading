7f28fca6d4e1bafb1fb3457415b0b1d28b750938
hyq <1713877106@qq.com>
2019-02-21 17:00:45 +0800

    v1.3.2

--- data/posts-data.js
+++ data/posts-data.js
@@ -12,9 +12,9 @@ var local_database = [{
     detail: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，壳凸红脂块块香”；在《世说新语》里，晋毕卓更是感叹“右手持酒杯，左手持蟹螯，拍浮酒船中，便足了一生矣。”漫漫人生长路，美食与爱岂可辜负？于是作为一个吃货，突然也很想回味一下属于我的味蕾记忆。记忆中的秋蟹，是家人的味道，弥漫着浓浓的亲情。\n\n是谁来自山川湖海，却囿于昼夜，厨房与爱？ 是母亲，深思熟虑，聪明耐心。吃蟹前，总会拿出几件工具，煞有介事而乐此不疲。告诉我们螃蟹至寒，需要佐以姜茶以祛寒，在配备的米醋小碟里，亦添入姜丝与紫苏，前者驱寒后者增香。泡好菊花茶，岁月静好，我们静等。",
     postId: 0,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
-      title: "夜夜夜夜-齐秦",
-      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+      url: "http://fs.w.kugou.com/201902211508/79fb67fc49dfb9d92c732cd89072cea6/G129/M02/0A/19/YZQEAFxtCqeACu-WACyetiUuWI8965.mp3",
+      title: "失去之后-夏婉安",
+      coverImg: "https://imgessl.kugou.com/stdmusic/20190220/20190220155506303633.jpg"
     }
   },
   {
@@ -31,9 +31,9 @@ var local_database = [{
     avatar: "/images/avatar/1.png",
     postId: 1,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38",
-      title: "鬼迷心窍-李宗盛",
-      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000"
+      url: "http://fs.w.kugou.com/201902211518/42d9f8a554894f2bf98d8da094b3a8b8/G128/M05/07/05/wA0DAFxqIJmAZIgpAELwnvE5ZkM335.mp3",
+      title: "Koi",
+      coverImg: "https://imgessl.kugou.com/stdmusic/20190220/20190220161108637720.jpg"
     }
   },
   {
@@ -51,9 +51,9 @@ var local_database = [{
     avatar: "/images/avatar/3.png",
     postId: 2,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100004HLusI2lLjZy.m4a?fromtag=38",
-      title: "女儿情-万晓利",
-      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
+      url: "http://fs.w.kugou.com/201902211453/d0ddd5a4ea86e30446210233392f300a/G111/M06/1D/10/D4cBAFoL9VyASCmXADTAFw14uaI428.mp3",
+      title: "生僻字",
+      coverImg: "https://imgessl.kugou.com/stdmusic/20171115/20171115124042700588.jpg"
     }
   },
   {
@@ -70,9 +70,9 @@ var local_database = [{
     avatar: "/images/avatar/5.png",
     postId: 3,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100002mWVx72p8Ugp.m4a?fromtag=38",
-      title: "恋恋风尘-老狼",
-      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000",
+      url: "http://fs.w.kugou.com/201902211506/8e10928c4f29636a2adc1159e7e84f75/G085/M07/0B/10/lQ0DAFujV42AK4xpACkHR2d9qTo587.mp3",
+      title: "一曲相思",
+      coverImg: "https://imgessl.kugou.com/stdmusic/20180920/20180920161445159306.jpg",
     }
   },
   {
@@ -89,9 +89,9 @@ var local_database = [{
     avatar: "../../../images/avatar/3.png",
     postId: 4,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
-      title: "沉默是金-张国荣",
-      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000"
+      url: "http://fs.w.kugou.com/201902211544/a5d868a087c1bf71506afb1588580914/G111/M05/17/0B/rw0DAFwIn1iAYYk-ACxJLShPlN0554.mp3",
+      title: "狂浪",
+      coverImg: "https://imgessl.kugou.com/stdmusic/20181206/20181206120211470480.jpg"
     }
   },
   {
@@ -108,9 +108,9 @@ var local_database = [{
     avatar: "../../../images/avatar/5.png",
     postId: 5,
     music: {
-      url: "http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38",
-      title: "朋友-谭咏麟",
-      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
+      url: "http://fs.w.kugou.com/201902211534/4b5a85db0af321c01d2fe39d836f8bec/G142/M08/05/03/bpQEAFvRdgKAPj-GAC5mMI81sKc401.mp3",
+      title: "夜之光",
+      coverImg: "https://imgessl.kugou.com/stdmusic/20181025/20181025155058473338.jpg"
     }
   },
 ]
--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -2,7 +2,8 @@ var postsData = require("../../../data/posts-data.js")
 
 Page({
   data: {
-    currentPostId:'',
+    currentPostId: '',
+    isPlayingMusic: false,
     // postData:{},
     // collect: false,
   },
@@ -10,8 +11,8 @@ Page({
   onLoad: function(options) {
     var postId = options.id;
     this.data.currentPostId = postId;
-    var postData = postsData.postList[postId];
 
+    var postData = postsData.postList[this.data.currentPostId];
     this.setData({
       postData: postData
     })
@@ -22,11 +23,13 @@ Page({
     if (postsCollected && postsShare) {
       var postCollected = postsCollected[postId];
       var postShare = postsShare[postId];
+      postCollected = false;
+      postShare = false;
       this.setData({
         collected: postCollected,
         share: postShare,
       })
-    }else{
+    } else {
       var postsCollected = {};
       var postsShare = {};
       postsCollected[postId] = false;
@@ -36,7 +39,7 @@ Page({
     }
   },
 
-  onCollectTap: function(event){
+  onCollectTap: function(event) {
     var postsCollected = wx.getStorageSync('posts_collected');
     var postCollected = postsCollected[this.data.currentPostId];
     // 收藏变为未收藏 未收藏变为收藏
@@ -46,19 +49,116 @@ Page({
     wx.setStorageSync('posts_collected', postsCollected)
     // 更新数据绑定
     this.setData({
-      collected:postCollected
+      collected: postCollected
+    })
+    wx.showToast({
+      title: postCollected ? '已收藏' : '取消收藏',
+      icon: postCollected ? 'success' : 'none',
+      duration: 1000,
+      mask: true,
     })
   },
 
-  onShareTap:function(event){
+  onShareTap: function(event) {
     var postsShare = wx.getStorageSync('posts_share');
     var postShare = postsShare[this.data.currentPostId];
     postShare = !postShare;
     postsShare[this.data.currentPostId] = postShare;
+
+    this.showModal(postShare, postsShare);
+
+    // wx.setStorageSync('posts_share', postsShare);
+    // this.setData({
+    //   share: postShare
+    // })
+    // wx.showToast({
+    //   title: postShare ? '已分享' : '取消分享',
+    //   icon: postShare ? 'success' : 'none',
+    //   duration: 1000,
+    //   mask: true,
+    // })
+    // wx.showModal({
+    //   title: '分享',
+    //   content: '是否分享该文章',
+    //   cancelText:"取消分享",
+    //   cancelColor:'#333',
+    //   confirmText:"分享",
+    //   confirmColor:"#405f80",
+    // })
+
+
+    // var itemList=[
+    //   "分享到微信好友",
+    //   "分享到朋友圈",
+    //   "分享到QQ",
+    //   "分享到微博",
+    // ];
+    // wx.showActionSheet({
+    //   itemList: itemList,
+    //   itemColor:"#405f80",
+    //   success:function(res){
+    //     // res.errMsg 用户是否点击取消按钮
+    //     // res.tapIndex 数组元素的序号，从零开始
+    //     wx.showModal({
+    //       title: '用户' + itemList[res.tapIndex],
+    //       content: "用户是否取消？" + res.errMsg+'暂时无法实现分享',
+    //     })
+    //   },
+    // })
+  },
+
+  showToast: function() {
     wx.setStorageSync('posts_share', postsShare);
     this.setData({
       share: postShare
     })
+    wx.showToast({
+      title: postShare ? '已分享' : '取消分享',
+      icon: postShare ? 'success' : 'none',
+      duration: 1000,
+      mask: true,
+    })
+  },
+
+  showModal: function(postShare, postsShare) {
+    var that = this;
+    wx.showModal({
+      title: '分享',
+      content: postShare ? '分享该文章' : '取消分享该文章',
+      cancelText: "取消",
+      cancelColor: '#333',
+      confirmText: "确认",
+      confirmColor: "#405f80",
+      success: function(res) {
+        if (res.confirm) {
+          wx.setStorageSync('posts_share', postsShare);
+          that.setData({
+            share: postShare
+          })
+        }
+      }
+    })
+  },
+
+  onMusicTap: function(event) {
+    var postData = postsData.postList[this.data.currentPostId];
+    // console.log(postData);
+    var isPlayingMusic = this.data.isPlayingMusic;
+    if (isPlayingMusic) {
+      wx.pauseBackgroundAudio();
+      this.setData({
+        isPlayingMusic: false
+      })
+    } else {
+      wx.playBackgroundAudio({
+        dataUrl: postData.music.url,
+        title: postData.music.title,
+        coverImgUrl: postData.music.coverImg,
+      })
+      this.setData({
+        isPlayingMusic:true
+      })
+    }
   },
 
 })
