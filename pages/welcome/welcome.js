db7ab0d20de827699fcf9e6005077caf920f8965
hyq <1713877106@qq.com>
2019-03-04 13:39:55 +0800

    v1.5.3

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -6,12 +6,15 @@ Page({
   },
 
   onTap: function () {
-    console.log("正在为您跳转！")
-    // 父子级页面跳转
-    wx.navigateTo({
-      url: '../reader/reader',
-      // url: "../posts/post",
+    console.log("正在为您跳转！");
+    wx.switchTab({
+      url: "../posts/post"
     });
+    // 父子级页面跳转
+    // wx.navigateTo({
+    //   url: '../reader/reader',
+    //   // url: "../posts/post",
+    // });
 
     // 平级页面跳转
     // wx.redirectTo({
